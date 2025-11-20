062adb2e1c32cc8e6c4b36739e305bdf514bb197
Fahim Ahmed <ahmedfahimwork@gmail.com>
2025-11-20 23:55:02 +0530

    increased blur

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className="p-4 h-full w-full">
             <div className="flex justify-center items-center w-full h-full relative">
-                <img src="./au.png" alt="augate" className="rounded-2xl blur-[8px] ... max-h-full max-w-full object-contain" />
+                <img src="./au.png" alt="augate" className="rounded-2xl blur-[16px] ... max-h-full max-w-full object-contain" />
                 <p className="text-5xl absolute top-[10%] left-[50%] -translate-x-[50%] -translate-y-[50%] font-bold">Find What’s Lost. Return What’s Found.</p>
                 <p className="text-xl absolute top-[20%] left-[50%] -translate-x-[50%] -translate-y-[50%] font-semibold w-[50%] text-center">A secure and reliable platform to report, search, and reconnect lost items with their rightful owners.</p>
                 <div className="absolute bottom-[5%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex justify-center items-center gap-30">
